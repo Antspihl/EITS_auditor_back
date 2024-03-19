599dc3088f47be6fc2ed08784174a6c24f7368e7
inpihl <antspihl@gmail.com>
2024-03-19 18:04:46 +0200

    fixed a small typo

--- src/main/java/ee/taltech/EITS_auditor_back/controller/CheckController.java
+++ src/main/java/ee/taltech/EITS_auditor_back/controller/CheckController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
 public class CheckController {
