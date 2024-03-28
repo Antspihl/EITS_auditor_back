ff6876ff5bc53a4a59be8d119019b462932ca880
inpihl <antspihl@gmail.com>
2024-03-28 15:35:07 +0200

    typo fix

--- src/main/java/ee/taltech/EITS_auditor_back/dto/response/Sys223M18DTO.java
+++ src/main/java/ee/taltech/EITS_auditor_back/dto/response/Sys223M18DTO.java
@@ -2,13 +2,13 @@ package ee.taltech.EITS_auditor_back.dto.response;
 
 public record Sys223M18DTO(
         boolean allRemoteAssistanceRulesAreAllowed,
-        boolean RemoteAssistanceDCOMInTCPNoScopeActive,
-        boolean RemoteAssistanceRAServerInTCPNoScopeActive,
-        boolean RemoteAssistancePnrpSvcUDPInEdgeScope,
-        boolean RemoteAssistancePnrpSvcUDPInEdgeScopeActive,
-        boolean RemoteAssistanceSSDPSrvInUDPActive,
-        boolean RemoteAssistanceInTCPEdgeScope,
-        boolean RemoteAssistanceSSDPSrvInTCPActive,
-        boolean RemoteAssistanceInTCPEdgeScopeActive
+        boolean remoteAssistanceDCOMInTCPNoScopeActive,
+        boolean remoteAssistanceRAServerInTCPNoScopeActive,
+        boolean remoteAssistancePnrpSvcUDPInEdgeScope,
+        boolean remoteAssistancePnrpSvcUDPInEdgeScopeActive,
+        boolean remoteAssistanceSSDPSrvInUDPActive,
+        boolean remoteAssistanceInTCPEdgeScope,
+        boolean remoteAssistanceSSDPSrvInTCPActive,
+        boolean remoteAssistanceInTCPEdgeScopeActive
         ) {
 }
--- src/main/java/ee/taltech/EITS_auditor_back/dto/response/Sys223M19DTO.java
+++ src/main/java/ee/taltech/EITS_auditor_back/dto/response/Sys223M19DTO.java
@@ -2,8 +2,8 @@ package ee.taltech.EITS_auditor_back.dto.response;
 
 public record Sys223M19DTO(
         boolean allRDPRulesAreAllowed,
-        boolean RemoteDesktopShadowInTCP,
-        boolean RemoteDesktopUserModeInTCP,
-        boolean RemoteDesktopUserModeInUDP
+        boolean remoteDesktopShadowInTCP,
+        boolean remoteDesktopUserModeInTCP,
+        boolean remoteDesktopUserModeInUDP
 ) {
 }
